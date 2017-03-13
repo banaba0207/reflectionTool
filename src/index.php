f54d561dd2ad04aba752c598443155d03b005a6d
banaba0207 <banaba0207@gmail.com>
2017-03-13 23:18:57 +0900

    First commit

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<?php
+    var_dump($_POST);
+?>
+
+
+<!DOCTYPE html>
+<html lang = “ja”>
+<head>
+<meta charset = “UFT-8”>
+<title>reflection</title>
+</head>
+<body>
+<h1>Let's Reflection</h1>
+<form action="" method="post">
+    <input name="task"/>
+    <button type="submit">更新</button>
+</form>
+
+</body>
+</html>
