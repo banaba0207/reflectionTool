231ccf8680d92a0cc5f858fbb7631c389d64d3c1
banaba0207 <banaba0207@gmail.com>
2017-03-25 14:34:18 +0900

    ドキュメントルートをpublic_htmlに設定

--- src/public_html/index.php
+++ src/public_html/index.php
@@ -1,5 +1,16 @@
 <?php
+
+// エラー出力する場合
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+// タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
+// composerのautoload読み込み
+require_once dirname(__DIR__) . "/vendor/autoload.php";
+
 require_once __DIR__ . '/Dispatcher.php';
 $dispatcher = new Dispatcher();
-$dispatcher->setSystemRoot(__DIR__ . '/..');
+$dispatcher->setSystemRoot(dirname(__DIR__));
 $dispatcher->dispatch();
