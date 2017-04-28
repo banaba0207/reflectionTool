1a91149a6342c552fb8facf9c9143ba4bd147d9c
takuro-yamaguchi <takuro_yamaguchi@pokelabo.co.jp>
2017-04-29 00:07:42 +0900

    Update TaskData.sql

--- src/sql/TaskData.sql
+++ src/sql/TaskData.sql
@@ -1,10 +1,9 @@
 CREATE TABLE TaskData (
-taskDataId unsigned int(11) NOT NULL AUTO_INCREMENT,
-task VARCHAR(100),
-isClosed unsigned int(1) DEFAULT 0,
-startTime timestamp DEFAULT CURRENT_TIMESTAMP,
-endTime timestamp ON UPDATE CURRENT_TIMESTAMP,
+taskDataId  unsigned int(11) NOT NULL AUTO_INCREMENT,
+task        VARCHAR(100),
+isClosed    unsigned int(1) DEFAULT 0,
+isCutInTask unsigned int(1) DEFAULT 0,
+startTime   timestamp DEFAULT CURRENT_TIMESTAMP,
+endTime     timestamp ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY (taskDataId)
 );
-
-CREATE TABLE TaskData (taskDataId INT(11) NOT NULL AUTO_INCREMENT, task VARCHAR(100),isClosed INT(1) DEFAULT 0, startTime timestamp DEFAULT CURRENT_TIMESTAMP,endTime timestamp ON UPDATE CURRENT_TIMESTAMP,PRIMARY KEY (taskDataId));
