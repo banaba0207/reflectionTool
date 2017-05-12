9da32277ac16db7c340c3833b58e1f5ae84c3cdd
banaba0207 <banaba0207@gmail.com>
2017-05-13 01:24:19 +0900

    deploy.php test2

--- src/deploy.php
+++ src/deploy.php
@@ -2,10 +2,10 @@
 echo "Test pull";
 $cmd = "cd ~/work/reflectionTool/ &&"
     . "git pull origin develop &&"
-    . ""
+    . "ls"
     . ""
     . ""
     ;
-exec('/usr/bin/git pull', $op, $rv);
+exec($cmd, $op, $rv);
 print_r($op);
 print_r($rv);
