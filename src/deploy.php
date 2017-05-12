cef8a070e16f999cdc551a3e61de3cef0d14e719
banaba0207 <banaba0207@gmail.com>
2017-05-13 02:47:08 +0900

    deploy.php test3

--- src/deploy.php
+++ src/deploy.php
@@ -1,10 +1,9 @@
 <?php
 echo "Test pull";
+$cpFileList = ['controller', 'deploy.php', 'logic', 'model', 'view', 'public_html', 'index.html'];
 $cmd = "cd ~/work/reflectionTool/ &&"
     . "git pull origin develop &&"
-    . "ls"
-    . ""
-    . ""
+    . "cp -r " . implode(" ", $cpFileList) . " /var/www/html/ "
     ;
 exec($cmd, $op, $rv);
 print_r($op);
